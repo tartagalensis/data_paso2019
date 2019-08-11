db1f32180a558b74f863d0d2a1afb29096bea5c6
Ervin Wilneder <ervinwilneder@gmail.com>
2019-08-11 00:11:56 -0300

    Add files via upload

--- get_data_Paso2019_fromGithub.R
+++ get_data_Paso2019_fromGithub.R
@@ -6,6 +6,7 @@ library('dplyr')
 
 branch = 'master' # master implica tener los datos más recientes, caso contrario agregar el timestamp deseado (ver en GitHub). Ej: 'master/100819-220057'
 
+# importar los datos del repo en github
 git_postulaciones = paste('https://raw.githubusercontent.com/ervinwilneder/data_paso2019/', branch, '/descripcion_postulaciones.dsv', sep = '')
 git_regiones = paste('https://raw.githubusercontent.com/ervinwilneder/data_paso2019/', branch, '/descripcion_regiones.dsv', sep = '')
 git_totales = paste('https://raw.githubusercontent.com/ervinwilneder/data_paso2019/', branch, '/medios_totales.dsv', sep = '')
@@ -14,6 +15,7 @@ git_totales_agrp_nac = paste('https://raw.githubusercontent.com/ervinwilneder/da
 git_totales_lista = paste('https://raw.githubusercontent.com/ervinwilneder/data_paso2019/', branch, '/medios_totales_lista.dsv', sep = '')
 git_distritos = 'https://raw.githubusercontent.com/ervinwilneder/data_paso2019/master/descripcion_distritos.csv'
 
+# crear tablas con cada uno de los archivos .dsv y .csv
 descripcion_postulaciones = read_delim(git_postulaciones, '|', col_names = TRUE)
 descripcion_regiones = read_delim(git_regiones, '|', col_names = TRUE)
 medios_totales = read_delim(git_totales, '|', col_names = TRUE)
@@ -22,11 +24,13 @@ medios_totales_agrp_politica_nac = read_delim(git_totales_agrp_nac, '|', col_nam
 medios_totales_lista = read_delim(git_totales_lista, '|', col_names = TRUE)
 distritos = read_delim(git_distritos, ';' , col_names = TRUE)
 
+# crear algunas subtablas para luego agregar los datos
 regiones = rename(descripcion_regiones, CODIGO_SECCION = "CODIGO_REGION")
 agrupacion = select(descripcion_postulaciones, CODIGO_AGRUPACION, NOMBRE_AGRUPACION) %>% distinct
 categoria = select(descripcion_postulaciones, CODIGO_CATEGORIA, NOMBRE_CATEGORIA) %>% distinct
 lista = select(descripcion_postulaciones, CODIGO_LISTA, NOMBRE_LISTA) %>% distinct
 
+# crear tabla de totales por agrupacion politica
 totales_agrp_politica = left_join(medios_totales_agrp_politica, agrupacion, by = NULL) %>% 
   left_join(categoria, by = NULL) %>% 
   left_join(distritos, by = NULL) %>% 
@@ -35,6 +39,7 @@ totales_agrp_politica = totales_agrp_politica %>%
   select(NOMBRE_AGRUPACION, NOMBRE_CATEGORIA, NOMBRE_DISTRITO, NOMBRE_REGION, VOTOS_AGRUPACION, VOTOS_VALIDOS)
 View(totales_agrp_politica)
 
+# crear tabla de totales por agrupacion politica - sólo nivel nacional
 totales_agrp_politica_nac = left_join(medios_totales_agrp_politica_nac, agrupacion, by = NULL) %>% 
   left_join(categoria, by = NULL) %>% 
   left_join(descripcion_regiones, by = NULL)
@@ -42,13 +47,7 @@ totales_agrp_politica_nac = totales_agrp_politica_nac %>%
   select(NOMBRE_AGRUPACION, NOMBRE_CATEGORIA, NOMBRE_REGION, VOTOS_AGRUPACION, VOTOS_VALIDOS)
 View(totales_agrp_politica_nac)
 
-totales = left_join(medios_totales, categoria, by = NULL) %>% 
-  left_join(regiones, by = NULL) %>% 
-  left_join(distritos, by = NULL)
-totales = totales %>% 
-  select(NOMBRE_CATEGORIA, NOMBRE_REGION, NOMBRE_DISTRITO, MESAS_HABILITADAS, MESAS_COMPUTADAS, CONTADOR, VALOR)
-View(totales)
-
+# crear tabla de totales por lista
 totales_lista = left_join(medios_totales_lista, lista, by = NULL) %>% 
   left_join(agrupacion, by = NULL) %>% 
   left_join(categoria, by = NULL) %>% 
@@ -57,3 +56,11 @@ totales_lista = left_join(medios_totales_lista, lista, by = NULL) %>%
 totales_lista = totales_lista %>% 
   select(NOMBRE_LISTA, NOMBRE_AGRUPACION, NOMBRE_CATEGORIA, NOMBRE_REGION, NOMBRE_DISTRITO, VOTOS_LISTA)
 View(totales_lista)
+
+# crear tabla de totales
+totales = left_join(medios_totales, categoria, by = NULL) %>% 
+  left_join(regiones, by = NULL) %>% 
+  left_join(distritos, by = NULL)
+totales = totales %>% 
+  select(NOMBRE_CATEGORIA, NOMBRE_REGION, NOMBRE_DISTRITO, MESAS_HABILITADAS, MESAS_COMPUTADAS, CONTADOR, VALOR)
+View(totales)
